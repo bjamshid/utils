c104f9c6323c43498b5e61a089bbf0ca3bb3a253
bjamshid <bjamshid@users.noreply.github.com>
2023-06-09 00:26:16 -0400

    Update and rename detect_change.sh to notify_on_change.sh

---
+++ notify_on_change.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <command_to_execute> <string_2_compare_with_command_output>"
+    echo "Example: $0 \"tail log.txt\" \"connected to localhost\""
+    exit 1
+fi
+
+# source telegram.sh to send notification when condition ini the loop is satisfied
+source telegram.sh
+
+# until [[ `w -h | awk '{print $3}' | sort | uniq | wc -l` -gt 1 ]]
+while [[ `$1` == *"$2"* ]]
+do
+    sleep 10
+done
+
+to_tg_bot $TGBOTTOKEN "$(hostname)" "command $1 doesn't contain $2 anymore!"
